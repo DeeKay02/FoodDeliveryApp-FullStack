4fe204ebce7f61c1ccbc78ac6a9a8431bce6fcca
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-09-30 19:19:28 +0530

    Add restaurant card

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 // Components
 import DeliveryCarousel from "./DeliveryCarousel";
+import Brand from "./Brand";
+import RestaurantCard from "../RestaurantCard";
 
 const Delivery = () => {
     return (
         <>
             <DeliveryCarousel />
+            {/* <Brand /> */}
+            <RestaurantCard />
         </>
     );
 };
