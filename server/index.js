cd126a52232852bc8031623d81a38bc0f26d6cf0
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-10-01 22:46:58 +0530

    Add image uploading route

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ import googleAuthConfig from "./config/google.config";
 import Auth from "./API/Auth";
 import Restaurant from "./API/Restaurant";
 import Food from "./API/Food";
+import Image from "./API/Image";
+import Menu from "./API/Menu";
 
 // Database connection
 import ConnectDB from "./database/connection";
@@ -35,6 +37,8 @@ googleAuthConfig(passport);
 zomato.use("/auth", Auth);
 zomato.use("/restaurant", Restaurant);
 zomato.use("/food", Food);
+zomato.use("/image", Image);
+zomato.use("/menu", Menu);
 
 zomato.get("/" , (req, res) => res.json({message: "Setup success"}));
 
