57b484044c1f0aa238b7d2d8cab65378add9a99e
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-10-04 15:58:35 +0530

    Add nutrition page banner

---
+++ client/src/Components/Nutrition/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// components
+import NutritionCarousal from "./NutritionCarousel";
+
+const Nutrition = () => {
+  return (
+    <div>
+      <NutritionCarousal />
+    </div>
+  );
+};
+
+export default Nutrition;
