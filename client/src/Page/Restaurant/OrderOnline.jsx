230ecf12cc6919569be6c76ac416571a0082393a
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-10-04 22:32:11 +0530

    Add food list

--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -6,29 +6,51 @@ import { BiTimeFive } from "react-icons/bi";
 import FloatMenuBtn from "../../Components/Restaurant/Order-Online/FloatMenuBtn";
 import MenuListContainer from "../../Components/Restaurant/Order-Online/MenuListContainer";
 import FoodItem from "../../Components/Restaurant/Order-Online/FoodItem";
+import FoodList from "../../Components/Restaurant/Order-Online/FoodList";
 
 const OrderOnline = () => {
   return (
     <>
-      <div className="w-full flex">
+      <div className="w-full h-screen flex">
         <aside className="hidden md:flex flex-col gap-3 border-r overflow-y-scroll border-gray-200 h-screen w-1/4">
-          <MenuListContainer />
-          <MenuListContainer />
+            <MenuListContainer />
+            <MenuListContainer />
         </aside>
-        <div className="w-full md:w-3/4">
-          <h2 className="text-xl font-semibold">Order Online</h2>
-          <h4 className="flex items-center gap-2 font-light text-gray-500">
-            <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
-          </h4>
-          <section>
-            <FoodItem
-              image="https://b.zmtcdn.com/data/dish_photos/839/62575c08ce26635e3f48b3642805d839.jpg?output-format=webp"
-              price="1000"
-              rating={3}
-              description="Tender mutton marinated with Andhra spices and cooked with long grain basmati rice. Served with raita, brinjal curry and bread halwa."
-              title="Andhra Mutton Biryani Family Pack"
-            />
-          </section>
+        <div className="w-full  px-3 md:w-3/4">
+            <div className="pl-3 mb-4">
+                <h2 className="text-xl font-semibold">Order Online</h2>
+                <h4 className="flex items-center gap-2 font-light text-gray-500">
+                <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
+                </h4>
+            </div>
+            <section className="flex  h-screen overflow-y-scroll flex-col gap-3 md:gap-5">
+                <FoodList
+                    title="Recommended"
+                    items={[
+                        {
+                        price: "1000",
+                        rating: 3,
+                        description: "Tender mutton marinated with Andhra spices and cooked with long grain basmati rice. Served with raita, brinjal curry and bread halwa.",
+                        title: "Andhra Mutton Biryani Family Pack",
+                        image:
+                            "https://b.zmtcdn.com/data/dish_photos/839/62575c08ce26635e3f48b3642805d839.jpg?output-format=webp",
+                        },
+                    ]}
+                />
+                <FoodList
+                    title="Pizza"
+                    items={[
+                        {
+                        price: "1000",
+                        rating: 3,
+                        description: "Tender mutton marinated with Andhra spices and cooked with long grain basmati rice. Served with raita, brinjal curry and bread halwa.",
+                        title: "Andhra Mutton Biryani Family Pack",
+                        image:
+                            "https://b.zmtcdn.com/data/dish_photos/839/62575c08ce26635e3f48b3642805d839.jpg?output-format=webp",
+                        },
+                    ]}
+                />
+            </section>
         </div>
       </div>
       <FloatMenuBtn />
