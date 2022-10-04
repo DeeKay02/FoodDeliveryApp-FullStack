8226e23e6eb8e41a4790f7c29b359053129e42d6
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-10-04 21:41:23 +0530

    Add menu float for restaurant in phone view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Temp from "./Components/temp";
 // Pages
 import Home from "./Page/Home";
 import Overview from "./Page/Restaurant/Overview";
+import OrderOnline from "./Page/Restaurant/OrderOnline";
 
 function App() {
   return (
@@ -31,7 +32,7 @@ function App() {
       <RestaurantLayoutHOC
         path="/restaurant/:id/order-online"
         exact
-        component={Temp}
+        component={OrderOnline}
       />
       <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Temp} />
       <RestaurantLayoutHOC
---
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// components
+import FloatMenuBtn from "../../Components/Restaurant/Order-Online/FloatMenuBtn";
+
+const OrderOnline = () => {
+  return (
+    <>
+      <div className="w-full">SD</div>
+      <FloatMenuBtn />
+    </>
+  );
+};
+
+export default OrderOnline;
