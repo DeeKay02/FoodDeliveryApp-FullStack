a390ec419eeee0829e945812acd9486f787425db
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-10-04 19:35:21 +0530

    Add restaurant layout

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { TiStarOutline } from "react-icons/ti";
+import { RiDirectionLine, RiShareForwardLine } from "react-icons/ri";
+import { BiBookmarkPlus } from "react-icons/bi";
 
 // Components
 import RestaurantNavbar from "../Components/Navbar/restaurantNavbar";
 import ImageGrid from "../Components/Restaurant/ImageGrid";
+import InfoButtons from "../Components/Restaurant/InfoButtons";
+import RestaurantInfo from "../Components/Restaurant/RestaurantInfo";
 
 const RestaurantLayout = () => {
   return (
@@ -19,6 +24,27 @@ const RestaurantLayout = () => {
             "https://b.zmtcdn.com/data/pictures/2/18621252/f737723f080910e46c451c51b9bbd717.jpg?output-format=webp",
           ]}
         />
+        <RestaurantInfo
+          name="Mumbai Xpress"
+          restaurantRating="3.5"
+          deliveryRating="3.2"
+          cuisine="North Indian, Fast Food, Chinese, Street Food"
+          address="Basavanagudi, Bangalore"
+        />
+        <div className="my-4 flex flex-wrap gap-3">
+          <InfoButtons isActive>
+            <TiStarOutline /> Add Review
+          </InfoButtons>
+          <InfoButtons>
+            <RiDirectionLine /> Direction
+          </InfoButtons>
+          <InfoButtons>
+            <BiBookmarkPlus /> Bookmark
+          </InfoButtons>
+          <InfoButtons>
+            <RiShareForwardLine /> Share
+          </InfoButtons>
+        </div>
       </div>
     </>
   );
