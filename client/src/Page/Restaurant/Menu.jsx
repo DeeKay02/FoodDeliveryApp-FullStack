03fdfc4c610ff3d181a60b1e3551ef06b8a1c90a
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-10-06 18:37:39 +0530

    Add menu and photos

--- client/src/Page/Restaurant/Menu.jsx
+++ client/src/Page/Restaurant/Menu.jsx
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // Component
 import MenuCollection from "../../Components/Restaurant/MenuCollection";
 
+// Redux actions
+import { getImage } from "../../Redux/Reducer/Image/Image.action";
+
+
 const Menu = () => {
   const [menus, setMenus] = useState([]);
 
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (reduxState) {
+      dispatch(getImage(reduxState?.menuImage)).then((data) => {
+        const images = [];
+        data.payload.image.images.map(({ location }) => images.push(location));
+        setMenus(images);
+      });
+    }
+  }, []);
+
   return (
     <>
-        <div className="flex flex-wrap gap-3">
-            {menus.map((menu) => (
-            <MenuCollection {...menu} />
-            ))}
-        </div>
+      <div className="flex flex-wrap gap-3">
+        <MenuCollection menuTitle="Menu" pages={menus.length} image={menus} />
+      </div>
     </>
   );
 };
