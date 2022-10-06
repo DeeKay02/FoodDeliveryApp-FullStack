c967dea9e29eb5af845b7b2ad82b57b86346b53c
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-10-06 20:09:45 +0530

    Add api for new restaurant data

--- server/API/Restaurant/index.js
+++ server/API/Restaurant/index.js
@@ -80,4 +80,16 @@ Router.get("/search", async (req, res) => {
     }
 });
 
+// @Route   POST /restaurants/new
+// @des     add new restaurant
+// @access  PRIVATE
+Router.post("/new", passport.authenticate("jwt"), async (req, res) => {
+    try {
+      const newRestaurant = await RestaurantModel.create(req.body.restaurantData);
+      return res.json({ restaurants: newRestaurant });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  });
+
 export default Router;
