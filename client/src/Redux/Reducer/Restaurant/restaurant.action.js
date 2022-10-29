b4d53b1e52ff290eb14a5192cd414ea31bf850a4
A Dhruva Krishnama Raju <dhruvakrishnamaraju@gmail.com>
2022-10-29 22:31:59 +0530

    Change city

--- client/src/Redux/Reducer/Restaurant/restaurant.action.js
+++ client/src/Redux/Reducer/Restaurant/restaurant.action.js
@@ -7,7 +7,7 @@ export const getRestaurant = () => async (dispatch) => {
   try {
     const restaurantList = await axios({
       method: "GET",
-      url: "http://localhost:4000/restaurant/?city=Bangalore",
+      url: "http://localhost:4000/restaurant/?city=Chennai",
     });
 
     return dispatch({ type: GET_RESTAURANT, payload: restaurantList.data });
